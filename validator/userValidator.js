c2661114478af27a1fae7d4310fcde9474064abe
Ritik Trivedi <ritiktrivedi902@gmail.com>
2025-05-07 10:20:08 +0530

    usermodel uservalidator and user routes updated

--- validator/userValidator.js
+++ validator/userValidator.js
@@ -13,9 +13,9 @@ const registerSchema = Joi.object({
 })
 
 const updateUserSchema = Joi.object({
-    username:Joi.string().required(),
-    email:Joi.string().email().required(),
-    password:Joi.string().required(),
+    username:Joi.string().optional(),
+    email:Joi.string().email().optional(),
+    password:Joi.string().optional(),
     isAdmin:Joi.boolean().optional()
 })
 
